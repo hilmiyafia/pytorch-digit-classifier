1c5dd6cf9a7a57149acbfe2eb077e6a23d5580d0
Hilmi Yafi A <56539775+hilmiyafia@users.noreply.github.com>
2026-04-24 22:46:33 +0700

    Added accuracy print out

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torchvision.transforms as transforms
 import torch.onnx
 import torch
 import numpy
+import tqdm
 import cv2
 import os
 
@@ -42,13 +43,27 @@ augment.append(transforms.RandomPerspective())
 augment.append(transforms.Resize((32, 32)))
 
 """ TRAINING """
-for j in range(1000):
+bar = tqdm.tqdm(range(1000))
+for j in bar:
     optimizer.zero_grad()
     output = model(augment(data))
     error = loss(output, target)
     error.backward()
-    print(f"Epoch {j}:", error.item())
+    bar.set_postfix(loss=f"{error.item():.4f}")
     optimizer.step()
+
+""" RESULTS """
+print("Confusion Matrix")
+matrix = numpy.zeros((10, 10))
+for j in range(1000):
+    output = model(augment(data))
+    index = output.argmax(1)
+    for l in range(10):
+        matrix[l, index[l].item()] += 1
+
+print(matrix)
+accuracy = numpy.sum(numpy.eye(10) * matrix) / numpy.sum(matrix)
+print(f"Accuracy: {accuracy * 100:.2f}%")
 
 """ EXPORT MODEL """
 dummy = torch.randn(1, 1, 32, 32)
