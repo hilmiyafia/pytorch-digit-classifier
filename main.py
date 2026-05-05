74e299bf175fa1ea3055dac8da0b4b324286ded9
Hilmi Yafi A <56539775+hilmiyafia@users.noreply.github.com>
2026-05-06 06:37:37 +0700

    Use onnxim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torchvision.transforms as transforms
+import subprocess
 import torchinfo
 import torch.onnx
 import torch
@@ -8,15 +9,10 @@ import cv2
 import os
 
 """ CREATE MODEl """
-model = torch.nn.Sequential()
-model.append(torch.nn.Conv2d(1, 3, 3))
-model.append(torch.nn.ReLU())
-model.append(torch.nn.MaxPool2d(2))
-model.append(torch.nn.Conv2d(3, 6, 3))
-model.append(torch.nn.ReLU())
-model.append(torch.nn.MaxPool2d(2))
-model.append(torch.nn.Conv2d(6, 10, 2))
-model.append(torch.nn.Flatten())
+model = torch.nn.Sequential(
+    torch.nn.Conv2d(1, 3, 3), torch.nn.ReLU(), torch.nn.MaxPool2d(2),
+    torch.nn.Conv2d(3, 6, 3), torch.nn.ReLU(), torch.nn.MaxPool2d(2),
+    torch.nn.Conv2d(6, 10, 2), torch.nn.Flatten()).cuda()
 torchinfo.summary(model, (1, 16, 16))
 
 """ LOAD DATA """
@@ -24,8 +20,7 @@ data = []
 for i in range(10):
     image = cv2.imread(f"images/{i}.png") / 255
     data.append([numpy.max(image, -1)])
-data = numpy.array(data)
-data = torch.tensor(data).type(torch.FloatTensor)
+data = torch.FloatTensor(numpy.stack(data)).cuda()
 
 """ CREATE OPTIMIZER """
 optimizer = torch.optim.Adagrad(model.parameters())
@@ -37,15 +32,15 @@ loss = torch.nn.CrossEntropyLoss()
 target = torch.arange(10, dtype=torch.long).cuda()
 
 """ CREATE AUGMENTATION FUNCTION """
-augment = torch.nn.Sequential()
-augment.append(transforms.RandomPerspective())
-augment.append(transforms.Resize((16, 16)))
+augment = torch.nn.Sequential(
+    transforms.RandomPerspective(p=0.2),
+    transforms.Resize((16, 16))).cuda()
 
 """ TRAINING """
 bar = tqdm.tqdm(range(1000))
 for j in bar:
     optimizer.zero_grad()
-    output = model(augment(data).cuda())
+    output = model(augment(data))
     error = loss(output, target)
     error.backward()
     bar.set_postfix(loss=f"{error.item():.4f}")
@@ -55,7 +50,7 @@ for j in bar:
 print("Confusion Matrix")
 matrix = numpy.zeros((10, 10))
 for j in range(1000):
-    output = model(augment(data).cuda())
+    output = model(augment(data))
     index = output.argmax(1)
     for l in range(10):
         matrix[l, index[l].item()] += 1
@@ -66,4 +61,6 @@ print(f"Accuracy: {accuracy * 100:.2f}%")
 
 """ EXPORT MODEL """
 dummy = torch.randn(1, 1, 16, 16)
-torch.onnx.export(model, dummy, "model.onnx", verbose=True)
+torch.onnx.export(model, dummy, "m.onnx", verbose=True)
+subprocess.call(["onnxsim", "m.onnx", "model.onnx"])
+os.remove("m.onnx")
